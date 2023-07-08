c91f1ab12154f54b2223bb1998aee5c3b0bb26eb
sreesai169 <sreesai169@gmail.com>
2023-07-08 13:31:50 +0530

    Committing terraform changes for creating instances

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
   name         = "test.test1"
-  type         = "String"
+  type         = "SecureString"
   value = "hello world"
+  key_id = "180e3e88-bcb5-48ea-a181-d7bfadff55a5"
 }
